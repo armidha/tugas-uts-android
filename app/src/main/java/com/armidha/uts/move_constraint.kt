15198c960290edfd76661da7e2709f558a2e13c1
armidha <rickyarmidha@gmail.com>
2019-04-18 19:15:00 +0700

    first

---
+++ app/src/main/java/com/armidha/uts/move_constraint.kt
@@ -0,0 +1,12 @@
+package com.armidha.uts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class move_constraint : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_constraint)
+    }
+}
